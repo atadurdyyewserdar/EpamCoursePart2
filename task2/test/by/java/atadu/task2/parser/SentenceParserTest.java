7e79e9a419cff1580d2faf0dd1371e36d9794b0d
Atadurdyyew Serdar <atadurdyyewserdar@gmail.com>
2020-04-20 08:32:37 +0300

    update

--- task2/test/by/java/atadu/task2/parser/SentenceParserTest.java
+++ task2/test/by/java/atadu/task2/parser/SentenceParserTest.java
@@ -5,14 +5,14 @@ import by.java.atadu.task2.composite.Composite;
 import by.java.atadu.task2.composite.Leaf;
 import by.java.atadu.task2.composite.Type;
 import org.testng.Assert;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SentenceParserTest {
     private String sentence;
     private Component expected;
 
-    @BeforeTest
+    @BeforeMethod
     public void setUp() {
         sentence = "Hello, my name is Serdar.";
         expected = new Composite(Type.SENTENCE);
